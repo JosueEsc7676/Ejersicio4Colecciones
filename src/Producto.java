6c225489a0a8d366de3510fefbca094a24f004e1
Josue Escobar <josueescobar000@hotmail.com>
2025-06-03 09:47:48 -0600

    ejercisio terminado

--- src/Producto.java
+++ src/Producto.java
@@ -5,7 +5,7 @@ public class Producto {
     private String marca;
     private double precio;
 
-    // Constructor
+
     public Producto(int id, String nombre, String marca, double precio) {
         this.id = id;
         this.nombre = nombre;
@@ -14,7 +14,6 @@ public class Producto {
         this.codigo = generarCodigo(nombre, marca);
     }
 
-    // Método para generar el código
     private String generarCodigo(String nombre, String marca) {
         String parteNombre = nombre.length() >= 3 ? nombre.substring(0, 3).toUpperCase() : nombre.toUpperCase();
         String parteMarca = marca.length() >= 2 ? marca.substring(0, 2).toUpperCase() : marca.toUpperCase();
@@ -22,7 +21,6 @@ public class Producto {
         return parteNombre + parteMarca + numero;
     }
 
-    // Getters y Setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
@@ -37,7 +35,6 @@ public class Producto {
     public double getPrecio() { return precio; }
     public void setPrecio(double precio) { this.precio = precio; }
 
-    // Mostrar datos
     @Override
     public String toString() {
         return "ID: " + id +
